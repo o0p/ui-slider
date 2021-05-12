24ed5d1229b6c6afb135fd7de15e7082049dd3db
Ruslan <rus@gmx.com>
2021-05-12 23:51:13 +0100

    Add article return with img and name

--- src/App.js
+++ src/App.js
@@ -16,6 +16,12 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
+          //more stuff coming
+          return (
+            <article key={id}>
+              <img src={image} alt={name} />
+            </article>
+          );
         })}
       </div>
     </section>
