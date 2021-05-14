db5fcff70cca487e97ec02bca2579ec27abfc3c7
Ruslan <rus@gmx.com>
2021-05-14 16:51:58 +0100

    Add btns listeners

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
@@ -36,10 +37,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
